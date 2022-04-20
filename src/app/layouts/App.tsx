0508075915f539512608775ac92a7aa4b747c6ae
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-21 01:26:33 +0700

    add component ToastContainer, add Switch for not load more than 1 route

--- src/app/layouts/App.tsx
+++ src/app/layouts/App.tsx
@@ -5,10 +5,12 @@ import NavBar from '../../features/nav/NavBar';
 import styled from 'styled-components';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
-import { Route, RouteComponentProps, withRouter } from 'react-router-dom';
+import { Route, RouteComponentProps, Switch, withRouter } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import NotFound from './NotFound';
+import { ToastContainer } from 'react-toastify';
 
 const ContainerStyled = styled(Container)`
   margin-top: 7em;
@@ -17,18 +19,22 @@ const ContainerStyled = styled(Container)`
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <>
+    <ToastContainer position="bottom-right" />
       <Route path='/' exact component={HomePage} />
       <Route path={'/(.+)'} render={() => (
         <>
           <NavBar />
           <ContainerStyled>
-            <Route path='/activities' exact component={ActivityDashboard} />
-            <Route path='/activities/:id' component={ActivityDetails} />
-            <Route
-              path={['/createActivity', '/manage/:id']}
-              key={location.key}
-              component={ActivityForm}
-            />
+            <Switch>
+              <Route path='/activities' exact component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route
+                path={['/createActivity', '/manage/:id']}
+                key={location.key}
+                component={ActivityForm}
+              />
+              <Route component={NotFound} />
+            </Switch>
           </ContainerStyled>
         </>
       )}
