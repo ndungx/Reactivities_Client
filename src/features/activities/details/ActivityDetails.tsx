c7901b3c7b213b6b0f3620765c03df5abe66d3ec
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-21 01:19:43 +0700

    Update ActivityDetails.tsx

--- src/features/activities/details/ActivityDetails.tsx
+++ src/features/activities/details/ActivityDetails.tsx
@@ -21,7 +21,9 @@ const ActivityDetails: React.FC<RouteComponentProps<DetailParams>> = ({ match })
         loadActivity(match.params.id);
     }, [loadActivity, match.params.id]);
 
-    if (loadingInitial || !activity) return <LoadingComponent content='Loading activity...' />
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />
+
+    if (!activity) return <h2>Activity not found</h2>
 
     return (
         <Grid>
