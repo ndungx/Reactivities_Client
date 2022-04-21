b6aba54823bfd4a02d6921129a6088eb7121bafd
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-21 19:50:35 +0700

    format date to human readable

--- src/features/activities/details/ActivityDetailedHeader.tsx
+++ src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,5 +1,7 @@
+import { format } from 'date-fns';
 import { observer } from 'mobx-react-lite';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Button, Header, Item, Segment, Image } from 'semantic-ui-react'
 import styled from 'styled-components';
 import { IActivity } from '../../../app/models/activity';
@@ -31,7 +33,7 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({ activity })
                                     content={activity.title}
                                     style={{ color: 'white' }}
                                 />
-                                <p>{activity.date}</p>
+                                <p>{format(activity.date, 'eeee do MMMM')}</p>
                                 <p>
                                     Hosted by <strong>Bob</strong>
                                 </p>
@@ -43,7 +45,7 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({ activity })
             <Segment clearing attached='bottom'>
                 <Button color='teal'>Join Activity</Button>
                 <Button>Cancel attendance</Button>
-                <Button color='orange' floated='right'>
+                <Button as={Link} to={`/manage/${activity.id}`} color='orange' floated='right'>
                     Manage Event
                 </Button>
             </Segment>
