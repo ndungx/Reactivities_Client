79fa23e4529a5c11cfdb391ae10cbf92044925cb
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-22 21:51:42 +0700

    add login and register button

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom'
 import { Button, Container, Header, Segment, Image } from 'semantic-ui-react'
 import styled from 'styled-components';
+import { RootStoreContext } from '../../app/stores/rootStore';
+import LoginForm from '../user/LoginForm';
+import RegisterForm from '../user/RegisterForm';
 
 const SegmentStyled = styled(Segment)`
   display: flex;
@@ -20,6 +24,10 @@ const HeaderTwoStyled = styled(Header)`
 `
 
 const HomePage = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { isLoggedIn, user } = rootStore.userStore;
+  const { openModal } = rootStore.modalStore;
+
   return (
     <SegmentStyled inverted textAlign='center' vertical className='masthead' >
       <Container text>
@@ -27,10 +35,34 @@ const HomePage = () => {
           <Image size='massive' src='/assets/logo.png' alt='logo' style={{ marginBottom: 12 }} />
           Reactivities
         </HeaderOneStyled>
-        <HeaderTwoStyled inverted content='Welcome to Reactivities' />
-        <Button as={Link} to='/activities' size='huge' inverted style={{ maginLeft: '0.5em' }}>
-          Take me to the activities!
-        </Button>
+        {isLoggedIn && user ? (
+          <>
+            <HeaderTwoStyled inverted content={`Welcome back ${user.displayName}`} />
+            <Button as={Link} to='/activities' size='huge' inverted style={{ maginLeft: '0.5em' }}>
+              Go to activities
+            </Button>
+          </>
+        ) : (
+          <>
+            <HeaderTwoStyled inverted content='Welcome to Reactivities' />
+            <Button
+              onClick={() => openModal(<LoginForm />)}
+              size='huge'
+              inverted
+              style={{ maginLeft: '0.5em' }}
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => openModal(<RegisterForm />)}
+              size='huge'
+              inverted
+              style={{ maginLeft: '0.5em' }}
+            >
+              Register
+            </Button>
+          </>
+        )}
       </Container>
     </SegmentStyled>
   )
