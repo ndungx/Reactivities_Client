48d5023b449c84b2529392c20f53d59a02ad0dc6
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-24 20:29:27 +0700

    add request postForm, add Profiles api
    add request postForm for send file

--- src/app/api/agents.ts
+++ src/app/api/agents.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import { history } from "../..";
 import { IActivity } from "../models/activity";
+import { IPhoto, IProfile } from "../models/profile";
 import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
@@ -42,7 +43,17 @@ const request = {
     get: (url: string) => axios.get(url).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(responseBody)
+    delete: (url: string) => axios.delete(url).then(responseBody),
+    postForm: async (url: string, file: Blob) => {
+        let formData = new FormData();
+
+        formData.append("File", file);
+
+        const response = await axios.post(url, formData, {
+            headers: { 'Content-type': 'multipart/form-data' }
+        });
+        return responseBody(response);
+    }
 }
 
 const Activities = {
@@ -61,9 +72,17 @@ const User = {
     register: (user: IUserFormValues): Promise<IUser> => request.post("/user/register", user)
 }
 
+const Profiles = {
+    get: (username: string): Promise<IProfile> => request.get(`/profiles/${username}`),
+    uploadPhoto: (photo: Blob): Promise<IPhoto> => request.postForm(`/photos`, photo),
+    setMainPhoto: (id: string) => request.post(`/photos/${id}/setmain`, {}),
+    deletePhoto: (id: string) => request.delete(`/photos/${id}`),
+}
+
 const exportedObject = {
     Activities,
     User,
+    Profiles,
 }
 
 export default exportedObject
