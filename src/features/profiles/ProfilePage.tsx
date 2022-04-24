1975a88ad51016cf202d61842a2d5dc936ecd9f7
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-24 20:31:04 +0700

    add profile page,

---
+++ src/features/profiles/ProfilePage.tsx
@@ -0,0 +1,36 @@
+import { observer } from 'mobx-react-lite'
+import React, { useContext, useEffect } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { Grid } from 'semantic-ui-react'
+import LoadingComponent from '../../app/layouts/LoadingComponent'
+import { RootStoreContext } from '../../app/stores/rootStore'
+import ProfileContent from './ProfileContent'
+import ProfileHeader from './ProfileHeader'
+
+interface RouteParams {
+    username: string;
+}
+
+interface IProps extends RouteComponentProps<RouteParams> { }
+
+const ProfilePage: React.FC<IProps> = ({ match }) => {
+    const rootStore = useContext(RootStoreContext);
+    const { profile, loadProfile, loadingProfile } = rootStore.profileStore;
+
+    useEffect(() => {
+        loadProfile(match.params.username);
+    }, [loadProfile, match])
+
+    if (loadingProfile) return <LoadingComponent content='Loading profile...' />
+
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader profile={profile!} />
+                <ProfileContent />
+            </Grid.Column>
+        </Grid>
+    )
+}
+
+export default observer(ProfilePage)
