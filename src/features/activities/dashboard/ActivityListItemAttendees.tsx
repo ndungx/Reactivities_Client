65b6a9b84cb80b8fe88588bc592ea8c83f204343
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-26 00:59:20 +0700

    update UI from getting data from api

--- src/features/activities/dashboard/ActivityListItemAttendees.tsx
+++ src/features/activities/dashboard/ActivityListItemAttendees.tsx
@@ -6,6 +6,11 @@ interface IProps {
     attendees: IAttendee[]
 }
 
+const styles = {
+    borderColor: 'orange',
+    borderWidth: 2,
+}
+
 const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
     return (
         <List horizontal>
@@ -19,6 +24,8 @@ const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
                                     size='mini'
                                     circular
                                     src={attendee.image || '/assets/user.png'}
+                                    bordered
+                                    style={attendee.following ? styles : null}
                                 />
                             }
                         />
--- src/features/profiles/ProfileContent.tsx
+++ src/features/profiles/ProfileContent.tsx
@@ -1,21 +1,27 @@
 import { Tab } from 'semantic-ui-react'
 import ProfileDesciption from './ProfileDesciption';
+import ProfileFollowings from './ProfileFollowing';
 import ProfilePhotos from './ProfilePhotos';
 
 const panes = [
     { menuItem: 'About', render: () => <ProfileDesciption /> },
     { menuItem: 'Photos', render: () => <ProfilePhotos /> },
     { menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane> },
-    { menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane> },
-    { menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane> },
+    { menuItem: 'Followers', render: () => <ProfileFollowings /> },
+    { menuItem: 'Following', render: () => <ProfileFollowings /> },
 ];
 
-const ProfileContent = () => {
+interface IProps {
+    setActiveTab: (activeIndex: any) => void;
+}
+
+const ProfileContent: React.FC<IProps> = ({ setActiveTab }) => {
     return (
         <Tab
             menu={{ fluid: true, vertical: true }}
             menuPosition='right'
             panes={panes}
+            onTabChange={(e, data) => setActiveTab(data.activeIndex)}
         />
     )
 }
